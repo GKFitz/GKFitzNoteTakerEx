d9135e773bf0b3bb32855ea3a23ae5c8c8edb8be
Gillian Fitzgerald <gillianfitzgerald@gillians-mbp.home>
2021-04-02 19:30:11 -0400

    added the / html rout, prepared for first  deployment

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -7,9 +7,14 @@ const path = require("path");
   // Below code handles when users "visit" a page.
   // In each of the below cases the user is shown an HTML page of content
 
+
 //calls for the notes input page
 router.get("/notes", function(req, res) {
     res.sendFile(path.join(__dirname, "../public/notes.html"));
+});
+
+router.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/index.html"));
 });
 
 //calls if there was no call to anything else it goes to the home page
